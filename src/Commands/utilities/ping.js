5ee438bb751d9e9de800048857db30e2226c2b64
TheAnnoying <81178023+TheAnnoying@users.noreply.github.com>
2024-04-19 16:07:03 +0300

    chore: made the ping command truly calculate the ping

--- src/Commands/utilities/ping.js
+++ src/Commands/utilities/ping.js
@@ -10,6 +10,22 @@ module.exports = {
 	 * @param {ChatInputCommandInteraction} interaction
 	 */
 	execute(interaction) {
-		interaction.reply({ content: "Pong!", ephemeral: true });
+		const before = Date.now();
+		await interaction.reply({ embeds: [
+		    new Discord.EmbedBuilder()
+		        .setTitle("Pinging...")
+		        .addFields({ name: "API Latency", value: client.ws.ping === -1 ? "Not calculated yet" : `${Math.round(client.ws.ping)} ms` })
+		        .setColor("#2b2d31")
+		], fetchReply: true });
+
+		interaction.editReply({ embeds: [
+		    new Discord.EmbedBuilder()
+		        .setTitle("Ping Results")
+		        .addFields(
+				{ name: "API Latency", value: client.ws.ping === -1 ? "Not calculated yet" : `${Math.round(client.ws.ping)} ms` },
+				{ name: "Bot Latency", value: `${Date.now() - before} ms` }
+			)
+		        .setColor("#2b2d31")
+		] });
 	}
 }
